bfe4a498c6e36277842f7a233c9b270cede112a3
JavatechWorld <105100691+JavatechWorld@users.noreply.github.com>
2023-10-18 20:34:48 +0530

    How to Expose Hello World API on Swagger
    How to Expose Hello World API on Swagger

---
+++ src/main/java/com/spring/implementation/SwaggerSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringBootApiApplication.class, args);
+	}
+
+}
